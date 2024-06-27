cc32810aaea470d3be59730cec2b70dc96bff5a0
os.environ[GITHUB_USERNAME] <os.environ[GITHUB_EMAIL]>
2024-06-27 20:49:30 +0000

    upd

--- src/main.py
+++ src/main.py
@@ -203,7 +203,7 @@ def run_experiment(id, args) :
 
 def run_path1(args) : 
 
-    learning_algorithms = ['fedavg'] 
+    learning_algorithms = ['central'] 
     # if args.learning_algorithm == 'central' :
     #     learning_algorithms = ['central', 'local']
     # else : 
@@ -295,7 +295,7 @@ if __name__ == "__main__" :
     parser.add_argument('--dataset', default = 'cifar10', metavar='DATASET', help='Specify the dataset')  # Mandatory dataset argument
     parser.add_argument('--learning_algorithm', default='fedavg', help='central, local, fedavg, fedmd, fedakd')  # Optional learning_algorithm argument
     parser.add_argument('--proxy_data_size', type = int, default=5000, help='Number of epochs') # Optional epochs argument
-    parser.add_argument('--num_clients', type = int, default=7, help='Number of clients participating in FL')  # Optional num_clients argument
+    parser.add_argument('--num_clients', type = int, default=1, help='Number of clients participating in FL')  # Optional num_clients argument
     parser.add_argument('--local_size', type = int, default=3000, help='size of data for each client')  # Optional num_clients argument
     parser.add_argument('--batch_size', type = int, default=12, help='Batch size')  # Optional num_clients argument
     parser.add_argument('--rounds', type = int, default=30, help='Number of global') # Optional rounds argument
